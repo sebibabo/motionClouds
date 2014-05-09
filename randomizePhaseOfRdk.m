b4eb4c330f0f8412790a0ad4afce044f4e17266c
sebibabo <sebastian.bosse@gmail.com>
2014-05-09 10:39:57 -0700

    corrected a bug regarding the luminance

--- randomizePhaseOfRdk.m
+++ randomizePhaseOfRdk.m
@@ -2,7 +2,7 @@
 clear all
 addpath(genpath('/home/bosse/dev/matlab/0.1-bosse/functionPool'))
 addpath(genpath('/home/bosse/devmotionClouds/function'))
-dataPath = '/export/data/motionClouds/MOCO_lifetime_MatFiles_30Hz_8.6deg_sec_MoreFrm';
+dataPath = '/export/data/motionClouds/MOCO_coh_phaseRand_PowerDiva';
 
 listing = dir(fullfile(dataPath,'MOCO*mat'));
 
@@ -12,9 +12,9 @@ stimModRate    = 1 ;
 numberOfReps   = 12 ;
 framesPerMod   = frameRefresh/stimModRate/2 ; 
 
-offsetRdk      = 45;
+offsetRdk      = 15;
 
-for fileIdx = 1:1%length(listing)
+for fileIdx = 6:6%length(listing)
 tic
   % read MOCO
   load(fullfile(dataPath, listing(fileIdx).name)) ;
@@ -41,16 +41,24 @@ tic
     randPhase  = 2 * pi* rand(size(motionEnv));
     randPhase  = angle( fftn( real( ifftn( exp(1j*randPhase)))));
     stimPart   = real(ifftn(motionEnv.* exp(1j*randPhase))) ;
-    stimPart   = stimPart-mean(stimPart(:));
-    stimPart   = (.5* stimPart/max(abs(stimPart(:))) + .5) ;
+    
+    stimPart = stimPart-mean(stimPart(:));
+    stimPart = (.5* stimPart/max(abs(stimPart(:))) + .5) ;
+    
     if mod(repIdx,2)==0
       stimulus(:,:, framesPerMod*2*(repIdx-1)+[16:30] ) = stimPart(:,:,15:-1:1) ;
     else
       stimulus(:,:, framesPerMod*2*(repIdx-1)+[16:30] ) = stimPart ;
     end
-    
-    
   end
+  
+  stimulus = drawFixationCross(stimulus,30);
+  images = single(reshape(stimulus,size(stimulus,1),size(stimulus,2),1,size(stimulus,3))) ;
+  temp = 1:frameRefresh*numberOfReps ;
+  temp = [temp; zeros( monitorRefresh/frameRefresh-1   ,length(temp))];
+  
+  imageSequence = uint32(temp(:) );
+  save(fullfile(dataPath,strrep(listing(fileIdx).name,'MOCO','MOCO_incohPhase')),'images','imageSequence');
 toc
 end
 
